c556c4939280287f02f4f9bbfc31a30f8e05c94d
gojkovicmatija99 <gojkovicmatija99@gmail.com>
2025-11-14 21:35:18 +0100

    v2

---
+++ v2/Resume-Nextjs/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
